800bbecaa5d6e304c26bd54a813af62800ce477b
Meleoro <arthur.perigno@gmail.com>
2022-03-21 12:40:52 +0100

    Début de scaraboy le pas gentil

---
+++ Insecte Superhéros/Assets/Scaraboy.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaraboy : MonoBehaviour
+{
+    [Header("Spawn")]
+    public float compteurDeclenchement;  // Nombre de mouches qu'il faut tuer pour le faire apparaitre
+    private bool stop;
+
+    [Header("Comportement")] 
+    public float vie;
+    public float speed;
+    public float hauteurMax;
+    public float hauteurMin;
+    private bool goDown;
+
+    [Header("Attaque")] 
+    public float frequenceAttaques;
+    public float pausesEntreAttaques;
+    public float cadenceDeTir;
+    private int numeroAttaque;
+    private float timer;
+    private float timerInterne;
+
+    [Header("Autres")] 
+    public Rigidbody2D rb;
+    public GestionEnnemis gestion;
+    public GameObject bullets;
+
+
+    private void Update()
+    {
+        Movements();
+        Shoot();
+    }
+
+    void Movements()
+    {
+        if (transform.position.y > hauteurMax)
+        {
+            rb.velocity = new Vector2(0, -speed);
+            goDown = true;
+        }
+        
+        else if (transform.position.y < hauteurMax && transform.position.y > hauteurMin && goDown == true)
+        {
+            rb.velocity = new Vector2(0, -speed);
+            goDown = true;
+        }
+        
+        else if (transform.position.y < hauteurMin)
+        {
+            rb.velocity = new Vector2(0, speed);
+            goDown = false;
+        }
+    }
+
+    void Shoot()
+    {
+        timer += Time.deltaTime;
+        if (timer > frequenceAttaques)
+        {
+            numeroAttaque = UnityEngine.Random.Range(0, 3);
+            timer = 0;
+            timerInterne = 0;
+        }
+
+        if (timer > pausesEntreAttaques)
+        {
+            timerInterne += Time.deltaTime;
+
+            if (numeroAttaque == 0 && timerInterne > cadenceDeTir)
+            {
+                timerInterne = 0;
+                Instantiate(bullets, transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
+            }
+            
+            else if (numeroAttaque == 1 && timerInterne > cadenceDeTir)
+            {
+                timerInterne = 0;
+                Instantiate(bullets, transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
+            }
+            
+            else if (numeroAttaque == 2 && timerInterne > cadenceDeTir)
+            {
+                timerInterne = 0;
+                Instantiate(bullets, transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
+            }
+        }
+        
+    }
+}
